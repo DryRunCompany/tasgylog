a0276e492e4a044d9e87574b56e879bf7f5b6bb9
garafu <garafo@gmail.com>
2022-09-18 19:21:20 +0900

    Feature/push test (#4)
    * pushに変更
    
    * 修正
    
    * 対象ブランチの追加
    
    * コメント削除
    
    * Add s3 bucket info
    
    * Modify initial files
    
    * Add permisssion

--- 10_infra/main.tf
+++ 10_infra/main.tf
@@ -10,8 +10,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket  = "tastylog-tfstate-bucket"
-    key     = "tastylog-test.tfstate"
+    bucket  = "tastylog-dryrun-tfstate-bucket"
+    key     = "tastylog-dryrun.tfstate"
     region  = "ap-northeast-1"
     profile = "terraform"
   }
@@ -23,8 +23,6 @@ terraform {
 provider "aws" {
   profile = "terraform"
   region  = "ap-northeast-1"
-  # access_key = "AKIAIOSFODNN7EXAMPLE"
-  # secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 }
 
 provider "aws" {
--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # infra
 
+1. `tfstate ファイル` の保管先準備＆設定
+
+    以下のファイルにある設定（S3バケット名、キー名）を見直し。
+    必要に応じて AWS 上に対応する S3バケット を準備。
+
+    `/10_infra/main.tf`
+
+        ... 省略 ...
+
+          backend "s3" {
+            bucket  = "{バケット名}"
+            key     = "{キー名}"
+            region  = "ap-northeast-1"
+            profile = "terraform"
+          }
+        }
+
+        ... 省略 ...
+
+
 1. `terraform.tfvars` ファイルの準備
 
     以下のファイルを作成。
